9eae26337bcac3c7c40282e57ec23060a420df2e
Oscar Kremer <oscar.s.kremer@hotmail.com>
2022-10-02 20:13:22 -0300

    feat: add template from pymentor

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-     name='pymentor',  
-     version='0.1.1',
-     scripts=['pymentor/mentor.py', 'pymentor/error.py'] ,
+     name='pytweezer',  
+     version='0.0.1',
+     scripts=['pytweezer/mentor.py', 'pymentor/error.py'] ,
      author="Oscar Schmitt Kremer",
      author_email="oscar.s.kremer@hotmail.com",
-     description="A package to implement a computational model of Mentor didactic robotic arm.",
+     description="short description here",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/oscarkremer/pymentor",
+     url="https://github.com/oscarkremer/pytweezer",
      packages=find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3",
