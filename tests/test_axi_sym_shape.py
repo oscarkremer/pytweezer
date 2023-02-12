1eaf80f879eb3d989fdfa97334d6bae08c9bfc17
oscarkremer <oscar.s.kremer@hotmail.com>
2023-02-11 21:55:36 -0300

    test: add test for boundary points values and shape

--- tests/test_axi_sym_shape.py
+++ tests/test_axi_sym_shape.py
@@ -5,34 +5,33 @@ from pytweezer.shapes import Sphere
 def test_sphere_boundary_points():
     decimal = 3
     radius = 1 
-    shape = Sphere(radius)
-    np.testing.assert_array_almost_equal(shape.perimeter, 6.2832, decimal=decimal, err_msg='Error computing perimeter of sphere')
     numr = 100
+    shape = Sphere(radius)
     r, t, p, n_rho, n_theta, n_phi, ds = shape.boundary_points(npts=numr)
-'''
-  testCase.verifyThat(length(rtp(:, 1)), IsEqualTo(numr), ...
-      'Incorrect number of values returned');
+    np.testing.assert_array_almost_equal(shape.perimeter, 6.2832, decimal=decimal, err_msg='Error computing perimeter of sphere')
 
-  testCase.verifyThat(rtp(:, 1), IsEqualTo(ones(size(rtp(:, 1))), ...
-      'Within', AbsoluteTolerance(tol)), ...
-      'Incorrect values for r');
-
-  testCase.verifyThat(n(:, 1), IsEqualTo(ones(size(n(:, 1))), ...
-      'Within', AbsoluteTolerance(tol)), ...
-      'Incorrect values for r');
-def test_sphere_volume():
+def test_sphere_boundary_points_shape_rtp():
     decimal = 3
     radius = 1 
+    numr = 100
     shape = Sphere(radius)
-    np.testing.assert_array_almost_equal(shape.volume, 4.1888, decimal=decimal, err_msg='Error computing volume of sphere')
+    r, _, _, _, _, _, _ = shape.boundary_points(npts=numr)
+    assert r.shape[0]==numr, 'Error in the shape of the returned vector for radial coordinate'
 
-
-def test_sphere_inside():
-    radius = 1
+def test_sphere_boundary_points_values_r():
+    decimal = 3
+    radius = 1 
+    numr = 100
     shape = Sphere(radius)
-    x = np.append(0.5*1*np.random.rand(1, 3), 4.0)
-    y = np.append(0.5*1*np.random.rand(1, 3), 4.0)
-    z = np.append(0.5*1*np.random.rand(1, 3), 4.0)
-    for value, expected in zip(shape.inside_xyz(x,y,z), [True, True, True, False]):
-        assert value==expected, 'Error in inside_xyz method for Spherical Shape'
-'''    
+    r, _, _, _, _, _, _ = shape.boundary_points(npts=numr)
+    np.testing.assert_array_almost_equal(r, np.ones((r.shape)), 
+        decimal=decimal, err_msg='Error computing r vector in boundary_points')
+
+def test_sphere_boundary_points_values_n():
+    decimal = 3
+    radius = 1 
+    numr = 100
+    shape = Sphere(radius)
+    _, _, _, n_rho, _, _, _ = shape.boundary_points(npts=numr)
+    np.testing.assert_array_almost_equal(n_rho, np.ones((n_rho.shape)), 
+        decimal=decimal, err_msg='Error computing n_rho vector in boundary_points')
