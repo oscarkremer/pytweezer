302b461d4e3cdecba2176cf95708586b906f4190
oscarkremer <oscar.s.kremer@hotmail.com>
2023-02-12 13:55:22 -0300

    feat: implementation of laguerre utils function

---
+++ pytweezer/utils/laguerre.py
@@ -0,0 +1,16 @@
+def laguerre(p, l, X):
+    '''% LAGUERRE associated Laguerre function
+    %
+    % L = LAGUERRE(p,l,X) evaluate associated Laguerre function.
+    % p and l must be integer scalars greater than zero
+
+    % This file is part of the optical tweezers toolbox.
+    % See LICENSE.md for information about using/distributing this file.
+    '''
+    Lplt = np.zeros((x.size, max(p+1,2)))
+    Lplt[:, 0] 1
+    Lplt[:, 1] =  l - x + 1
+    for i in range(2, p+1):
+        Lplt[:,i] = 1/i*((2*i+l-1-x)*Lplt[:, i-1]-(i+l-1)*Lplt[:, i-2])
+    Lpl = Lplt[:, p].reshape(X.shape)
+    return Lpl
--- pytweezer/utils/paraxial_transformation_matrix.py
+++ pytweezer/utils/paraxial_transformation_matrix.py
@@ -1,180 +1,120 @@
+import numpy as np
+
+
 def paraxial_transformation_matrix(paraxial_order, basis_in, basis_out, normal_mode=0)
-    order = int(100*normal_mode)+10*basis_in[0] + basis_out[1])
+    order = int(100*normal_mode)+10*basis_in[0] + basis_out[0])
     if order == 0:
-        modeweights=genLG2IG(paraxial_order,0);
+        modeweights = genLG2IG(paraxial_order, 0)
     elif order == 1:
-        modeweights=genLG2HG(paraxial_order);
+        modeweights = genLG2HG(paraxial_order)
     elif order == 2:
-        modeweights=genLG2IG(paraxial_order,basis_out(2));
+        modeweights = genLG2IG(paraxial_order, basis_out[1])
     elif order == 10:
-        modeweights=genLG2IG(paraxial_order,0)*genLG2vHG(paraxial_order)';
-    case order == 11:
-        modeweights=genLG2HG(paraxial_order)*genLG2vHG(paraxial_order)';
-    case order == 12:
-        modeweights=genLG2IG(paraxial_order,basis_out(2))*genLG2vHG(paraxial_order)';
-    case 020
-        modeweights=genLG2IG(paraxial_order,0)*genLG2vIG(paraxial_order,basis_in(2))';
-    case 021
-        modeweights=genLG2HG(paraxial_order)*genLG2vIG(paraxial_order,basis_in(2))';
-    case 022
-        modeweights=genLG2IG(paraxial_order,basis_out(2))*genLG2vIG(paraxial_order,basis_in(2))';
-    case 100
-        modeweights=eye(paraxial_order+1);
-    case 101
-        modeweights=genLG2HG(paraxial_order)*genLG2IG(paraxial_order,0)';
-    case 102
-        modeweights=genLG2IG(paraxial_order,basis_out(2))*genLG2IG(paraxial_order,0)';
-    case 110
-        modeweights=genLG2IG(paraxial_order,0)*genLG2HG(paraxial_order)';
-    case 111
-        modeweights=genLG2HG(paraxial_order)*genLG2HG(paraxial_order)';
-    case 112
-        modeweights=genLG2IG(paraxial_order,basis_out(2))*genLG2HG(paraxial_order)';
-    case 120
-        modeweights=genLG2IG(paraxial_order,0)*genLG2IG(paraxial_order,basis_in(2))';
-    case 121
-        modeweights=genLG2HG(paraxial_order)*genLG2IG(paraxial_order,basis_in(2))';
-    case 122
-        modeweights=genLG2IG(paraxial_order,basis_out(2))*genLG2IG(paraxial_order,basis_in(2))';
-    otherwise
-        ott.warning('external');
-        error('unknown parameter')
-end
-
-
-switch basis_out(1)
-    case 0
-        i3_out=[];
-        i2_out=[-paraxial_order:2:paraxial_order].';
-        i1_out=floor((paraxial_order-abs(i2_out))/2);
-    case 1
-        i3_out=[];
-        i2_out=[0:paraxial_order].';
-        i1_out=paraxial_order-i2_out;   
-    case 2
-        i3_out=[0:paraxial_order].';
-        i2_out=paraxial_order-2*(floor(i3_out/2));
-        i1_out=paraxial_order*ones(paraxial_order+1,1);  
-        i3_out=ott.utils.iseven(i3_out);
-        
-end
-row_modes=[i1_out,i2_out,i3_out];
-
-
-switch basis_in(1)
-    case 0
+        modeweights = genLG2IG(paraxial_order, 0)*genLG2vHG(paraxial_order).T
+    elif order == 11:
+        modeweights = genLG2HG(paraxial_order)*genLG2vHG(paraxial_order).T
+    elif order == 12:
+        modeweights = genLG2IG(paraxial_order, basis_out[1])*genLG2vHG(paraxial_order).T
+    elif order == 20:
+        modeweights = genLG2IG(paraxial_order, 0)*genLG2vIG(paraxial_order, basis_in[1]).T
+    elif order == 21:
+        modeweights = genLG2HG(paraxial_order)*genLG2vIG(paraxial_order,basis_in[1]).T
+    elif order == 22:
+        modeweights = genLG2IG(paraxial_order,basis_out[1])*genLG2vIG(paraxial_order,basis_in[1]).T
+    elif order == 100:
+        modeweights = np.eye(paraxial_order+1)
+    elif order == 101:
+        modeweights = genLG2HG(paraxial_order)*genLG2IG(paraxial_order,0).T
+    elif order == 102:
+        modeweights = genLG2IG(paraxial_order,basis_out[1])*genLG2IG(paraxial_order,0).T
+    elif order == 110:
+        modeweights = genLG2IG(paraxial_order,0)*genLG2HG(paraxial_order).T
+    elif order == 111:
+        modeweights = genLG2HG(paraxial_order)*genLG2HG(paraxial_order).T
+    elif order == 112:
+        modeweights = genLG2IG(paraxial_order,basis_out[1])*genLG2HG(paraxial_order).T
+    elif order == 120:
+        modeweights = genLG2IG(paraxial_order,0)*genLG2IG(paraxial_order,basis_in[1]).T
+    elif order == 121:
+        modeweights = genLG2HG(paraxial_order)*genLG2IG(paraxial_order,basis_in[1]).T
+    elif order == 122:
+        modeweights = genLG2IG(paraxial_order,basis_out[1])*genLG2IG(paraxial_order,basis_in[1]).T
+    else:
+        raise ValueError('Unknown order')
+    if basis_out[0] == 0:
+        i3_out=[]
+        i2_out=[-paraxial_order:2:paraxial_order].T
+        i1_out=floor((paraxial_order-abs(i2_out))/2)
+    elif basis_out[0] == 1:
+        i3_out=[]
+        i2_out=[0:paraxial_order].T
+        i1_out=paraxial_order-i2_out
+    elif basis_out[0] == 2:
+        i3_out=[0:paraxial_order].T
+        i2_out=paraxial_order-2*(floor(i3_out/2))
+        i1_out=paraxial_order*ones(paraxial_order+1,1)
+        i3_out=ott.utils.iseven(i3_out)
+    row_modes=[i1_out,i2_out,i3_out]
+    if basis_in[0] == 0:
         i3_in=[];
-        i2_in=[-paraxial_order:2:paraxial_order].';
-        i1_in=floor((paraxial_order-abs(i2_in))/2);
-    case 1
+        i2_in=[-paraxial_order:2:paraxial_order].T
+        i1_in=floor((paraxial_order-abs(i2_in))/2)
+    if basis_in[0] == 1:
         i3_in=[];
-        i2_in=[0:paraxial_order].';
-        i1_in=paraxial_order-i2_in;   
-    case 2
-        i3_in=[0:paraxial_order].';
-        i2_in=paraxial_order-2*(floor(i3_in/2));
-        i1_in=paraxial_order*ones(paraxial_order+1,1);  
-        i3_in=ott.utils.iseven(i3_in);
+        i2_in=[0:paraxial_order].T
+        i1_in=paraxial_order-i2_in
+    if basis_in[0] == 2:
+        i3_in=[0:paraxial_order].T
+        i2_in=paraxial_order-2*(floor(i3_in/2))
+        i1_in=paraxial_order*ones(paraxial_order+1,1)
+        i3_in=ott.utils.iseven(i3_in)
 
-end
-col_modes=[i1_in,i2_in,i3_in];
+    col_modes=[i1_in,i2_in,i3_in]
+    return modeweights, col_modes, row_modes
 
-ott.warning('external');
+def genLG2HG(order_paraxial):
+    n=[0:floor(order_paraxial/2)];
+    k=n;
+    [N,K]=meshgrid(n,k);
+    M=order_paraxial-N;
+    P=min(N,M);
 
-    return [ modeweights col_modes row_modes ]
+    normalisation_matrix=sqrt(factorial(order_paraxial-K).*factorial(K)./factorial(N)./factorial(M)/2^order_paraxial);
 
-def [output,LGlookups,HGlookups]=genLG2HG(order_paraxial);
-% genLG2HG.m --- LG->HG conversion matrix.
-%
-% Usage:
-%
-% [modewieghts,LGlookups,HGlookups] = genLG2HG(paraxial_order)
+    summed_matrix=zeros(size(normalisation_matrix));
 
-n=[0:floor(order_paraxial/2)];
-k=n;
-[N,K]=meshgrid(n,k);
-M=order_paraxial-N;
-P=min(N,M);
+    summed_matrix(:,1)=factorial(M(:,1))./factorial(M(:,1)-K(:,1));
+    summed_matrix(1,:)=ones([1,floor(order_paraxial/2)+1]);
 
-%Let's start with the troublesome sum. We need to keep convergence... best
-%seems to recur with inner index. We only need (in the uppder block) to:
-%
-%   i=0:        S_i=m!n!/(m-k+i)!/(n-i)!nchoosek(k,i)
-%   0<i<n/2:    S_i=-(n-i)/(m-k+i+1)*i/(i+1)S_(i-1)
-%
-% which seems stable.
+    s_i=zeros(length(k)-1,floor(order_paraxial/2)+1);
 
-%normalisation matrix:
-normalisation_matrix=sqrt(factorial(order_paraxial-K).*factorial(K)./factorial(N)./factorial(M)/2^order_paraxial);
-
-summed_matrix=zeros(size(normalisation_matrix));
-
-%special cases
-summed_matrix(:,1)=factorial(M(:,1))./factorial(M(:,1)-K(:,1));
-summed_matrix(1,:)=ones([1,floor(order_paraxial/2)+1]);
-
-s_i=zeros(length(k)-1,floor(order_paraxial/2)+1);
-
-for jj=1:length(n)-1
-    mm=order_paraxial-jj;
-    for kk=1:length(k)-1
-        s_i(kk,1)=factorial(jj)*factorial(mm)/factorial(mm-kk)/factorial(jj)*nchoosek(kk,0);
-        
-        for ii=1:kk
-            
-            s_i(kk,ii+1)=-(jj-ii+1)/(mm-kk+ii)/(ii)*(kk-ii+1)*s_i(kk,ii);
-            
-        end
-        
-    end
-    summed_matrix(2:end,jj+1)=sum(s_i,2);
-end
-
-%we now multiply for upper block!
-block_to_mirror=normalisation_matrix.*summed_matrix./factorial(K).*(-1).^(P);
-
-output=zeros(order_paraxial+1);
-
-%we're going to produce the spinor inverse of what was in beijersbergen
-%1993.
-output(1:floor(order_paraxial/2)+1,:) = [block_to_mirror(:,1:ceil(order_paraxial/2)),fliplr(block_to_mirror.*(-1).^(P.'))]; %
-output(end-ceil(order_paraxial/2)+1:end,:)=flipud(output(1:ceil(order_paraxial/2),:));
-
-output(ceil(order_paraxial/2)+1:end,2:2:end)=-output(ceil(order_paraxial/2)+1:end,2:2:end);
-output(2:2:end,:)=-output(2:2:end,:);
-output=output.*(1i).^repmat([0:order_paraxial].',[1,order_paraxial+1]);
-
-if nargout>1
+    for jj=1:length(n)-1
+        mm=order_paraxial-jj;
+        for kk=1:length(k)-1
+            s_i(kk,1)=factorial(jj)*factorial(mm)/factorial(mm-kk)/factorial(jj)*nchoosek(kk,0);            
+            for ii=1:kk
+                s_i(kk,ii+1)=-(jj-ii+1)/(mm-kk+ii)/(ii)*(kk-ii+1)*s_i(kk,ii);
+        summed_matrix(2:end,jj+1)=sum(s_i,2);
+    block_to_mirror=normalisation_matrix.*summed_matrix./factorial(K).*(-1).^(P);
+    output=zeros(order_paraxial+1);
+    output(1:floor(order_paraxial/2)+1,:) = [block_to_mirror(:,1:ceil(order_paraxial/2)),fliplr(block_to_mirror.*(-1).^(P.'))]; %
+    output(end-ceil(order_paraxial/2)+1:end,:)=flipud(output(1:ceil(order_paraxial/2),:));
+    output(ceil(order_paraxial/2)+1:end,2:2:end)=-output(ceil(order_paraxial/2)+1:end,2:2:end);
+    output(2:2:end,:)=-output(2:2:end,:);
+    output=output.*(1i).^repmat([0:order_paraxial].T,[1,order_paraxial+1]);
     [LGlookups,HGlookups]=meshgrid([0:order_paraxial],[0:order_paraxial]);
-end
-end
+    return output, lg_lookups, hg_lookups
 
 
-function [output,LGlookups,HGlookups]=genLG2vHG(order_paraxial);
-% genLG2vHG.m --- LG->vortex HG conversion matrix.
-%
-% Usage:
-%
-% [modewieghts,LGlookups,HGlookups] = genLG2vHG(paraxial_order)
+def genLG2vHG(order_paraxial):
+    n=[0:floor(order_paraxial/2)]
+    k=n
+    N, K = np.meshgrid(n,k);
+    M = order_paraxial-N;
+    P = min(N,M)
+    normalisation_matrix=sqrt(factorial(order_paraxial-K).*factorial(K)./factorial(N)./factorial(M)/2^order_paraxial);
 
-n=[0:floor(order_paraxial/2)];
-k=n;
-[N,K]=meshgrid(n,k);
-M=order_paraxial-N;
-P=min(N,M);
-
-%Let's start with the troublesome sum. We need to keep convergence... best
-%seems to recur with inner index. We only need (in the uppder block) to:
-%
-%   i=0:        S_i=m!n!/(m-k+i)!/(n-i)!nchoosek(k,i)
-%   0<i<n/2:    S_i=-(n-i)/(m-k+i+1)*i/(i+1)S_(i-1)
-%
-% which seems stable.
-
-%normalisation matrix:
-normalisation_matrix=sqrt(factorial(order_paraxial-K).*factorial(K)./factorial(N)./factorial(M)/2^order_paraxial);
-
-summed_matrix=zeros(size(normalisation_matrix));
+    summed_matrix=zeros(size(normalisation_matrix));
 
 %special cases
 summed_matrix(:,1)=factorial(M(:,1))./factorial(M(:,1)-K(:,1));
