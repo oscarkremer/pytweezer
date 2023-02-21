fix: correction in creation of Gaussian beam for computation of components

pytweezer/beams/beam.py
@@ -34,9 +34,9 @@ class Beam:
     def make_beam_vector(self, a, b, n, m, n_max=None):
         if not n_max:
             if not n.size:
-                n_max = 0
+                self.n_max = 0
             else:
-                n_max = n.max()
+                self.n_max = n.max()
         total_orders = combined_index(n_max, n_max)
         ci = combined_index(n, m)
         n_beams = a.shape[1] if len(a.shape) > 1 else 1
@@ -63,26 +63,62 @@ class Beam:
 
 
     def append(self, other):
-        if self.n_beams == 0:
+        if self.n_beams == 1:
             # DANGER: Have to implement this section
-            beam = other
+            self.a = other.a
+            self.b = other.b
         else:
             self.n_max = max(self.n_max, other.n_max)
             other.n_max = self.n_max
-            self.a = np.concatenate(self.a, other.a)
-            self.b = np.concatenate(self.b, other.b)
+            self.a = np.concatenate([self.a, other.a])
+            self.b = np.concatenate([self.b, other.b])
 
+    def __mul__(self, other):
+        print('here')
+        if isinstance(other, (int, float)):
+            self.a = other*self.a
+            self.b = other*self.b
+            return self
+        elif isinstance(other, np.ndarray):
+            if other.shape[0] == 2*self.a.shape[0]:
+                print(self.a.shape)
+                print(self.b.shape)
+                ab = np.matmul(other, np.concatenate([self.a, self.b]))
+                self.a = ab[:int(ab.shape[0]/2),:]
+                self.b = ab[int(ab.shape[0]/2):,:]
+                return self
+        elif isinstance(other, complex):
+            if hasattr(other, '__iter__'):
+                if other.shape[0] == 2*self.a.shape[0]:
+                    print(self.a.shape)
+                    print(self.b.shape)
+                    ab = other*np.concatenate([self.a, self.b])
+                    return self
+        else:
+            #raise #TypeError('The multiplication operation is not possible for the type of variable used!')
+            return NotImplemented
 
-    # THIS IS AN ADDITIONAL GATE-KEEPING METHOD TO ENSURE 
-    # THAT EVEN WHEN PROPERTIES ARE DELETED, CLONED OBJECTS
-    # STILL GETS DEFAULT VALUES (NONE, IN THIS CASE)
-    def normalizeArgs(self):
-        if not hasattr(self, "a"):
-            self.a      = None
-        if not hasattr(self, "b"):
-            self.b      = None
-        if not hasattr(self, "kwargs"):
-            self.kwargs = {}
+    def __rmul__(self, other):
+        print('here', other)
+        if isinstance(other, (int, float)):
+            self.a = other*self.a
+            self.b = other*self.b
+        elif isinstance(other, np.ndarray):
+            if other.shape[0] == 2*self.a.shape[0]:
+                ab = np.matmul(other, np.concatenate([self.a, self.b]))
+                self.a = ab[:int(ab.shape[0]/2),:]
+                self.b = ab[int(ab.shape[0]/2):,:]
+                return self
+        elif isinstance(other, complex):
+            if hasattr(other, '__iter__'):
+                if other.shape[0] == 2*self.a.shape[0]:
+                    ab = np.matmul(other, np.concatenate([self.a, self.b]))
+                    self.a = ab[:int(ab.shape[0]/2),:]
+                    self.b = ab[int(ab.shape[0]/2):,:]
+                    return self
+        else:
+            #raise #TypeError('The multiplication operation is not possible for the type of variable used!')
+            return NotImplemented
 
 '''
  function data = GetVisualisationData(field_type, xyz, rtp, vxyz, vrtp)
@@ -1442,23 +1478,6 @@ class Beam:
       end
     end
 
-    function beam = mtimes(a,b)
-      if isa(a, 'ott.Bsc')
-        beam = a;
-        beam.a = beam.a * b;
-        beam.b = beam.b * b;
-      else
-        beam = b;
-        if size(a, 2) == 2*size(beam.a, 1)
-          ab = a * [beam.a; beam.b];
-          beam.a = ab(1:size(ab, 1)/2, :);
-          beam.b = ab(1+size(ab, 1)/2:end, :);
-        else
-          beam.a = a * beam.a;
-          beam.b = a * beam.b;
-        end
-      end
-    end
 
     function beam = plus(beam1, beam2)
       %PLUS add two beams together

pytweezer/beams/gaussian.py
@@ -30,7 +30,7 @@ class Gaussian(PointMatch):
         self.mode = mode
         self.offset = offset
         self.polarization = polarization
-
+        self.n_max = n_max
         if self.validate_translation_method(translation_method):
             self.translation_method = translation_method
         else:
@@ -58,7 +58,8 @@ class Gaussian(PointMatch):
             initial_mode = np.array([i1_out, i2_out]).T
         elif self.beam_function == 'ig':
             raise ValueError('Beam function not implemented yet!')
-        keepz = np.argwhere(np.abs(mode_weights[int(row-1),:]) > 0)
+        
+        keepz = np.where(np.abs(mode_weights[int(row-1),:]) > 0)
         initial_mode = initial_mode[keepz, :][0]
         c = mode_weights[int(row-1), keepz]
         self.angle = np.arcsin(na/self.index_m)
@@ -112,8 +113,6 @@ class Gaussian(PointMatch):
             norm_paraxial = np.sqrt(2*np.math.factorial(radial_mode)/(np.pi*np.math.factorial(radial_mode+np.abs(azimuthal_mode))))
             L = laguerre(int(radial_mode), int(np.abs(azimuthal_mode)), rw)
             mult1 = np.exp(-rw/2 + 1j*azimuthal_mode*phi+1j*np.pi/2*(radial_mode*2+np.abs(azimuthal_mode)+1))
-            elements = norm_paraxial*np.power(rw, np.abs(azimuthal_mode/2))*L*mult1
-            
             beam_envelope[:,i-1] = norm_paraxial*np.power(rw, np.abs(azimuthal_mode/2))*L*mult1
             mode_input_power = np.sqrt((2*np.pi*np.power(abs(beam_envelope[:, i-1]),2)*np.sqrt(rw/2)*np.abs(dr)).sum())
             aperture_power_normalization = np.sqrt(sum(2*np.pi*np.power(np.abs(beam_envelope[:,i-1]), 2)*np.sin(theta)))
@@ -123,7 +122,7 @@ class Gaussian(PointMatch):
             if conditional_args[0].size:
                 conditional_args = tuple(conditional_args[0].T)
                 mode_index_vector = list(conditional_args)+mode_index_vector
-        mode_index_vector = set(mode_index_vector)
+        mode_index_vector = sorted(set(mode_index_vector))
         beam_envelope = beam_envelope.sum(axis=1)
         outbeam = theta < np.pi - self.truncation_angle
         beam_envelope[outbeam] = 0
@@ -135,10 +134,9 @@ class Gaussian(PointMatch):
             beam_envelope = beam_envelope * phase_shift
         Ex = x_comp * beam_envelope * central_amplitude
         Ey = y_comp * beam_envelope * central_amplitude
-
         if azimuthal or radial:
-            E_theta = -radial*x_comp * beam_envelope * central_amplitude;
-            E_phi = azimuthal*y_comp * beam_envelope * central_amplitude;
+            E_theta = -radial*x_comp * beam_envelope * central_amplitude
+            E_phi = azimuthal*y_comp * beam_envelope * central_amplitude
         else:
             E_theta = - Ex * np.cos(phi) - Ey * np.sin(phi)
             E_phi = - Ex * np.sin(phi) + Ey * np.cos(phi)
@@ -150,28 +148,13 @@ class Gaussian(PointMatch):
             mm = mm[list(mode_index_vector)]
             mm = mm[abs(mm) <= paraxial_order+1]
             nn = nn[abs(mm) <= paraxial_order+1]
-            nn = np.sort(nn)
         a, b = self.bsc_far_field(nn, mm, e_field, theta, phi, zero_rejection_level= zero_rejection_level)
         self.a = a
         self.b = b
         self.power = power
         self.beam_type = 'incident'
         self.beam_basis = 'regular'
-
-    def translate_z(self, **kwargs):
-        if self.translation_method == 'default':        
-            A, B = translate_z(**kwargs)
-            return _, A, B
-        elif self.translation_method == 'new_beam_offset':
-            if not kwargs.get('z'):
-                kwargs['z'] = 0
-            if not kwargs.get('n_max'):
-                kwargs['n_max'] = self.n_max
-            return Gaussian(self.gtype, self.mode, 
-                offset=self.offset+np.array([0,0,kwargs['z']]),
-                omega=self.omega, power=self.power, lambda_m=self.lambda_b,
-                polarization=self.polarization, truncation_angle=self.truncation_angle,
-                n_max=self.n_max, angle=self.angle)
+        self.n_beams = 1
 
     def validate_beam_type(self, s):
         return True if s in ('lg', 'hg', 'ig') else False

pytweezer/beams/point_match.py
@@ -18,8 +18,10 @@ class PointMatch(Beam):
             for n in range(1, nn.max()+1):
                 ci = np.where(nn == n)[0]
                 _, dtY, dpY = spherical_harmonics(n, np.sort(mm[ci-1]), theta, phi)
-                coefficient_matrix[:,ci] = np.concatenate([dpY.T, -dtY.T]) * np.power(1j,(n+1))/np.sqrt(n*(n+1))
-                coefficient_matrix[:,ci+nn.size] = np.concatenate([dtY.T, dpY.T])*np.power(1j, n)/np.sqrt(n*(n+1))
+                dtY = dtY.T if dtY.shape[0] < dtY.shape[1] else dtY
+                dpY = dpY.T if dpY.shape[0] < dpY.shape[1] else dpY
+                coefficient_matrix[:,ci] = np.concatenate([dpY, -dtY]) * np.power(1j,(n+1))/np.sqrt(n*(n+1))
+                coefficient_matrix[:,ci+nn.size] = np.concatenate([dtY, dpY])*np.power(1j, n)/np.sqrt(n*(n+1))
             if invert_coefficient_matrix:
                 icm = pinv(coefficient_matrix)
             if invert_coefficient_matrix:
@@ -44,7 +46,7 @@ class PointMatch(Beam):
             mm = mm[pwr > zero_rejection_level*pwr.max()]
             fa = fa[pwr > zero_rejection_level*pwr.max()]
             fb = fb[pwr > zero_rejection_level*pwr.max()]
-        a, b, _, _ = self.make_beam_vector(fa, fb, nn, mm) 
+        a, b, _, _ = self.make_beam_vector(fa, fb, nn, mm, self.n_max) 
         return a, b
 '''  
 #s    def bsc_focalplane(self, nn, mm, e_field, kr, theta, phi, varargin):

pytweezer/beams/translation.py
@@ -1,42 +1,47 @@
+import numpy as np
+from copy import copy
+from pytweezer.utils import translate_z as translation
+
 def translate(beam, A, B):
-    beam = [ A, B, B, A ] * beam;
-    return beam
+    AB = np.block([[A, B], [B, A]])
+    return copy(beam)*AB
+
+def _translate_z_(beam, z=0):
+    def translate_z_type_helper(z, n_max, basis):
+        if basis == 'incoming':
+            translation_type = 'sbesselh2';
+        elif basis == 'outgoing':
+            translation_type = 'sbesselh1';
+        elif basis == 'regular':
+            translation_type = 'sbesselj'
+        A, B, _ = translation(n_max, z, function_type=translation_type)
+        return A, B
+    beam.dz = beam.dz + np.abs(z)
+    z = z * beam.k_m / 2 / np.pi
+    if isinstance(z, np.ndarray):
+        for i in range(1, z.size+1):
+            A, B = translate_z_type_helper(z[i], beam.n_max)
+            beam.append(translate(beam, A, B))
+            beam.beam_basis = 'regular'
+    else:
+        A, B = translate_z_type_helper(z, beam.n_max, beam.beam_basis)
+        beam.append(translate(beam, A, B))
+        beam.beam_basis = 'regular'
+    return beam, A, B
 
 
-def _translate_z(beam, z=0):
-beam.translation_method == 'default'"
-
-        if not kwargs.get('z'):
-            raise ValueError('No z informed for translation method!')
-        else:
-            z = kwargs['z']
-            beam.dz = beam.dz + np.abs(z)
-            z = z * beam.k_m / 2 / np.pi
-            i_beam = beam
-            if isinstance(z, np.ndarray):
-                for i in range(1, z.size+1):
-                    A, B = ibeam.translateZ_type_helper(z[i], [p.Results.Nmax, ibeam.Nmax]);
-                    beam = beam.append(ibeam.translate(A, B));
-                    beam.basis = 'regular';
-            else:
-                A, B = ibeam.translateZ_type_helper(z, [p.Results.Nmax, ibeam.Nmax]);
-                beam = beam.append(ibeam.translate(A, B))
-                beam.basis = 'regular';
-        return beam, A, B
-
-
-def translate_z(beam, **kwargs):
+def translate_z(beam, z, n_max=None):
     if beam.translation_method == 'default':        
-        A, B = _translate_z(beam, **kwargs)
-        return _, A, B
+        beam, A, B = _translate_z_(beam, z=z)
+        return beam, A, B
     elif beam.translation_method == 'new_beam_offset':
         if not kwargs.get('z'):
             kwargs['z'] = 0
         if not kwargs.get('n_max'):
-            kwargs['n_max'] = self.n_max
-        return Gaussian(beam.gtype, self.mode, 
-            offset=self.offset+np.array([0,0,kwargs['z']]),
-            omega=self.omega, power=self.power, lambda_m=self.lambda_b,
-            polarization=self.polarization, truncation_angle=self.truncation_angle,
-            n_max=self.n_max, angle=self.angle)
+            n_max = beam.n_max
+        return Gaussian(beam.gtype, beam.mode, 
+            offset=beam.offset+np.array([0,0,z]),
+            omega=beam.omega, power=beam.power, lambda_m=beam.lambda_b,
+            polarization=beam.polarization, truncation_angle=beam.truncation_angle,
+            n_max=n_max, angle=beam.angle), A, B
 

pytweezer/utils/spherical_harmonics.py
@@ -40,23 +40,21 @@ def spherical_harmonics(n, m, theta, phi=np.array([])):
     pnm = legendre_row(n, theta)
 
     pnm = pnm[abs(m), :]
-    
     phiM, mv = np.meshgrid(phi, m)
-    index_m_positive = np.argwhere(m>=0)
-    index_m_negative = np.argwhere(m<0)
-
-    pnm = np.concatenate([1/((-1)**(-mv[tuple(index_m_negative),:]))*pnm[tuple(index_m_negative),:], pnm[tuple(index_m_positive),:]])
-
+    index_m_positive = np.where(m>=0)
+    index_m_negative = np.where(m<0)
+    negative_pnm = ((-1)**(-mv[m<0,:]))*pnm[m<0,:]
+    pnm = np.concatenate([((-1)**(-mv[m<0,:]))*pnm[m<0,:], pnm[m>=0,:]])
     pnm = pnm.reshape((pnm.shape[0], -1))
     expphi = np.exp(1j*mv*phiM)
     Y = pnm*expphi
+#    print(Y.min(), Y.max(), Y.mean())
     expplus = np.exp(1j*phiM)
     expminus = np.exp(-1j*phiM)
 
     ymplus = np.concatenate([Y[1:,:], np.zeros((1,theta.shape[0]))])
     ymminus = np.concatenate([np.zeros((1,theta.shape[0])), Y[:-1,:]])
     Ytheta = np.sqrt((n-mv+1)*(n+mv))/2*expplus*ymminus - np.sqrt((n-mv)*(n+mv+1))/2*expminus*ymplus
-
     Y2 = _spherical_harmonics(n+1,theta,phi)
     ymplus = Y2[2:, :]
     ymminus = Y2[:-2,:]
@@ -104,13 +102,17 @@ def _spherical_harmonics(n, m, theta, phi=None):
     theta, phi = match_size(theta, phi)
     input_length = theta.shape[0]
     pnm = legendre_row(n, theta)
+
     pnm = pnm[abs(m), :] 
     phiM, mv = np.meshgrid(phi, m)
     index_m_positive = np.argwhere(m>=0)
     index_m_negative = np.argwhere(m<0)
-    pnm = np.concatenate([1/((-1)**(-mv[tuple(index_m_negative),:]))*pnm[tuple(index_m_negative),:], pnm[tuple(index_m_positive),:]])
+
+    pnm = np.concatenate([(-1)**(-mv[m<0,:])*pnm[m<0,:], pnm[m>=0,:]])
     
     pnm = pnm.reshape((pnm.shape[0], -1))
     expphi = np.exp(1j*mv*phiM)
+
     Y = pnm*expphi
+   # print(Y.min(), Y.max(), Y.mean())
     return Y

test_quick.py
@@ -1,12 +1,9 @@
 from pytweezer.shapes import Sphere
 import numpy as np
 from pytweezer.beams import Gaussian
+from pytweezer.beams.translation import translate_z, translate
 from pytweezer.utils import angular_grid, vswf_cart
 from numpy import matlib as matlib
-
+from copy import copy
 
 beam = Gaussian()
-beam.power = 1.0
-dz = np.pi/2
-tol = 1e-6
-beam.translate_z(z=dz)

tests/beams/test_bsc.py
@@ -1,12 +1,23 @@
 import pytest
 import numpy as np
 from pytweezer.beams import Gaussian
+from pytweezer.beams.translation import translate_z, translate
+from copy import copy
 
-def test_beam_translate():
-#    pass
-    beam = Gaussian()
-#    beam.power = 1.0
-#    dz = np.pi/2
+def test_beam_translate_z():
+    beam = Gaussian()    
+    beam.power = 1.0
+    dz = np.pi/2
+    decimal = 6
+    beam_2, A, B = translate_z(copy(beam), z=dz)
+    AB = np.block([[A, B], [B, A]])
+    beam_3 = copy(beam)*AB
+    beam_4 = translate(beam, A, B)
+    np.testing.assert_array_almost_equal(beam_2.a, beam_3.a, decimal=decimal, err_msg='Waist failed test for equality')
+    np.testing.assert_array_almost_equal(beam_2.a, beam_4.a, decimal=decimal, err_msg='Waist failed test for equality')
+    np.testing.assert_array_almost_equal(beam_2.b, beam_3.b, decimal=decimal, err_msg='Waist failed test for equality')
+    np.testing.assert_array_almost_equal(beam_2.b, beam_4.b, decimal=decimal, err_msg='Waist failed test for equality')
+
 
 #    translated_beam1 = beam.translate_z(z=dz)
 #    pass